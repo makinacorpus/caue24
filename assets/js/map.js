2c50aabd5b10b7a049b69b85dcd36d2526a49a1b
Simon Georges <simon.georges@makina-corpus.com>
2013-09-25 16:06:20 +0200

    Use position: absolute everywhere for consistent theming

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,11 +6,17 @@ var subDomains = ['otile1','otile2','otile3','otile4'];
 var mapquestAttrib = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors';
 L.tileLayer(mapquestUrl, {maxZoom: 15, attribution: mapquestAttrib, subdomains: subDomains}).addTo(map);
 // Add GeoJSON Layer
+function onEachFeature(feature, layer) {
+  if (feature.properties) {
+    layer.bindPopup("Altitude : " + feature.properties.ALTITUDE);
+  }
+}
 $.ajax({
   type: "GET",
+//  url: "http://localhost:4000/assets/test.geojson",
   url: "http://makinacorpus.github.io/caue24/assets/test.geojson",
   dataType: 'json',
   success: function (response) {
-    geojsonLayer = L.geoJson(response).addTo(map);
+    geojsonLayer = L.geoJson(response, {onEachFeature: onEachFeature}).addTo(map);
   }
 });
