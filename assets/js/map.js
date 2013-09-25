5e8d7b60f11217b710ec7dc36d56f9da98390f57
Simon Georges <simon.georges@makina-corpus.com>
2013-09-25 08:50:20 +0200

    Add GeoJSON point layer (planning to add dynamic data load events)

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,16 @@
+// Create base map
 var map = new L.Map('map').setView([45, 0.67], 10);
+// Add OSM Layer
 var mapquestUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';
 var subDomains = ['otile1','otile2','otile3','otile4'];
 var mapquestAttrib = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors';
 L.tileLayer(mapquestUrl, {maxZoom: 15, attribution: mapquestAttrib, subdomains: subDomains}).addTo(map);
+// Add GeoJSON Layer
+$.ajax({
+  type: "GET",
+  url: "http://makinacorpus.github.io/caue24/assets/test.geojson",
+  dataType: 'json',
+  success: function (response) {
+    geojsonLayer = L.geoJson(response).addTo(map);
+  }
+});
