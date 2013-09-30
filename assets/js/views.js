8b7c4f5eabd3313f2c9de0992349f5647b82b683
Simon Georges <simon.georges@makina-corpus.com>
2013-09-30 15:16:41 +0200

    HTML areas are now updated thanks to a Prose.io editable page

--- assets/js/views.js
+++ assets/js/views.js
@@ -1,10 +1,25 @@
 var CaueViews = {};
 
+CaueViews.displayData = function(layer, rawHtml, id) {
+  var dom$ = $(rawHtml);
+  // Parse data
+  var popupTitle = dom$.find('.popuptitle').text();
+  var popupContent = dom$.find('.popupcontent').text();
+  var additionalText = dom$.find('.additionaltext').text();
+  var photos = dom$.find('.images').text();
+  // Raise events
+  layer.bindPopup('<h2>'+ id + ': ' + popupTitle +'</h2>' + popupContent).openPopup();
+  $('#map-infos').html(id + ': ' + additionalText);
+  $('#map-photos').html(id + ': ' + photos);
+}
+
 CaueViews.clickLayer = function(layer, id) {
   // Get id Jekyll page
-  // Parse it
-  // Raise events
-  layer.bindPopup("Altitude : " + id);
-  $('#map-infos').html("Infos additionnelles " + id);
-  $('#map-photos').html("Photos " + id);
+  var featureId = id;
+  // Get page content
+//  var page = $.get("http://localhost:4000/data/test-page.html", function(data) {
+  var page = $.get("http://makinacorpus.github.com/caue24/data/test-page.html", function(data) {
+    // And parse it
+    CaueViews.displayData(layer, data, id);
+  });
 }
