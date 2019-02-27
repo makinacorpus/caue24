69e6af8852bf1c03469d542577b8d66ebb84ce6c
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-02-27 09:40:30 +0100

    Change puppeteer default viewport

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   launch: {
     headless: process.env.HEADLESS !== 'false',
+    defaultViewport: { width: 1240, height: 850 },
   },
   server: {
     command: 'serve -l 5555 -d _site',
